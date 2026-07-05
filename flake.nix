2fef77aa266b9cb690b219113fb3c93737b38828
datbtw <testchatgpt2904@gmail.com>
2026-07-05 23:37:45 +0700

    upgrade kernel cachyos

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, chaotic, ... }@inputs: {
     nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       
